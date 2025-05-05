c367935227445311d6077b790b65121b94e8c6a4
semtexfromua <125829448+semtexfromua@users.noreply.github.com>
2025-05-05 14:13:34 +0300

    Update pointer_to_caret_key.c

--- src/pointer_to_caret_key.c
+++ src/pointer_to_caret_key.c
@@ -1,5 +1,5 @@
-#include <zmk/input/input_processor.h>
-#include <zmk/input/events.h>
+#include <drivers/input_processor.h>
+#include <zephyr/input/input.h>
 #include <zmk/hid.h>
 #include <zmk/keymap.h>
 #include <zmk/endpoints.h>
@@ -9,9 +9,9 @@
 static int32_t caret_delta_x = 0;
 static int32_t caret_delta_y = 0;
 
-static int pointer_to_caret_key_process(const struct zmk_input_event *event) {
-    if (event->type != ZMK_INPUT_EVENT_TYPE_REL) {
-        return ZMK_INPUT_PROCESSOR_CONTINUE;
+static int pointer_to_caret_key_process(const struct device *dev, struct input_event *event, uint32_t param1, uint32_t param2, struct zmk_input_processor_state *state) {
+    if (event->type != INPUT_EV_REL) {
+        return ZMK_INPUT_PROC_CONTINUE;
     }
 
     caret_delta_x += event->rel.x;
@@ -39,7 +39,7 @@ static int pointer_to_caret_key_process(const struct zmk_input_event *event) {
     }
 
     // Гасимо pointer-подію, щоб вона не потрапила у HID-звіт миші
-    return ZMK_INPUT_PROCESSOR_STOP;
+    return ZMK_INPUT_PROC_STOP;
 }
 
 ZMK_INPUT_PROCESSOR_DEFINE(pointer_to_caret_key_process, pointer_to_caret_key); 
